7d351765320e5a0c1bc635ec819818bdce3d3cf0
dhyangabriel517-sys <dhyangabriel517@gmail.com>
2026-06-12 08:25:07 -0300

    Create Ativ.1.kt

---
+++ Ativ.1.kt
@@ -0,0 +1,4 @@
+1. As ferramentas de design,
+como Figma e Adobe XD, ajudam a criar protótipos, 
+organizar a interface e testar ideias antes da programação,
+facilitando o desenvolvimento do aplicativo. 
